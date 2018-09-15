0ee9b0abb01ff598d76552bc70348cc9f1f0a74f
austin <austin@dringo.io>
2018-09-15 13:16:31 -0700

    Repo initialized

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Main />
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
