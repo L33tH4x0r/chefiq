0ff682202f89544eaf4d36709aace6ebe0a427ea
austin <austin@dringo.io>
2018-09-16 12:14:37 -0700

    Router workgin

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom'
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Header />
-        <Main />
-      </BrowserRouter>
+      <Router>
+        <div>
+          <Header />
+          <Main />
+        </div>
+      </Router>
     );
   }
 }
+
+const Main = () => (
+  <main>
+    <Route exact path='/' component={Home}/>
+  </main>
+)
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+      </ul>
+    </nav>
+    <hr />
+  </header>
+)
+
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Tornadoes Website!</h1>
+  </div>
+)
 
 export default App;
