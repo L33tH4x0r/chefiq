57416266f66ffc35eab91f0399f4c3152bc97cc6
austin <aturner9967@gmail.com>
2018-11-03 08:05:40 -0700

    refactor: Moved header logic to own file

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,146 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 import {Header} from './Layouts'
-import Excersizes from './Excersizes'
-
-import PropTypes from 'prop-types';
-
-import { withStyles } from '@material-ui/core/styles';
-import {
-    SwipeableDrawer, AppBar, Toolbar, List, Typography, IconButton, Icon, Hidden,
-    Divider, ListItem, ListItemIcon, ListItemText, Button
-  } from '@material-ui/core';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import MenuIcon from '@material-ui/icons/Menu';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-
-const drawerWidth = 240;
-
-const styles = theme => ({
-  list:{
-    width: 250,
-  },
-  root: {
-    flexGrow: 1,
-  },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  appBar: {
-    marginLeft: drawerWidth,
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-    },
-  },
-  menuButton: {
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-  },
-});
-
 
 class App extends Component {
-  state = {
-    isOpen: false,
-  };
-
-  toggleDrawer = (open) => () => {
-    this.setState({
-      isOpen: open,
-    });
-  };
-
   render() {
     const classes = this.props;
-
     const Index = () => <h2>Home</h2>;
     const Secondary = () => <h2>Secondary</h2>;
-
-    const drawer = (
-        <Fragment>
-          <div className={classes.toolbar} />
-          <Divider />
-          <List>
-            <ListItem>
-              <ListItemIcon>{<InboxIcon />}</ListItemIcon>
-              <ListItemText>
-                <Link to="/">Home</Link>
-              </ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>{<MailIcon />}</ListItemIcon>
-              <ListItemText>
-                <Link to="/secondary">Secondary</Link>
-              </ListItemText>
-            </ListItem>
-          </List>
-        </Fragment>
-    );
 
     return (
       <div className={classes.root}>
         <Router>
           <Fragment>
-            <AppBar position="static">
-              <Toolbar>
-                <Container>
-                  <Row>
-                    <Col xs="1">
-                      <IconButton
-                        className={classes.menuButton, "float-left"}
-                        onClick={this.toggleDrawer(true)}
-                        color="inherit"
-                        aria-label="Menu">
-                        <MenuIcon />
-                      </IconButton>
-                    </Col>
-                    <Col xs="9">
-                      <Typography variant="h6" color="inherit" className={classes.grow, "text-center"}>
-                        ChefIQ
-                      </Typography>
-                    </Col>
-                    <Col xs="2">
-                      <Button color="inherit" className="float-right">Login</Button>
-                    </Col>
-                  </Row>
-                </Container>
-              </Toolbar>
-            </AppBar>
-            <SwipeableDrawer
-              open={this.state.isOpen}
-              onClose={this.toggleDrawer(false)}
-              onOpen={this.toggleDrawer(true)}>
-              <div
-                tabIndex={0}
-                role="button"
-                onClick={this.toggleDrawer(false)}
-                onKeyDown={this.toggleDrawer(false)}
-                >
-                {drawer}
-              </div>
-            </SwipeableDrawer>
+            <Header />
             <Route path="/" exact component={Index} />
             <Route path="/secondary" component={Secondary} />
           </Fragment>
@@ -150,23 +23,4 @@ class App extends Component {
   }
 }
 
-const Main = () => (
-  <main>
-    <h1>Chef IQ</h1>
-    <Route exact path='/' component={Catalogs}/>
-  </main>
-)
-
-const Catalogs = () => (
-  <div>
-    <h2>Catalogs</h2>
-  </div>
-)
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-  // Injected by the documentation to work in an iframe.
-  // You won't need it on your project.
-  container: PropTypes.object,
-  theme: PropTypes.object.isRequired,
-};
-export default withStyles(styles, { withTheme: true })(App);
+export default App;
--- client/src/Components/Layouts/header.js
+++ client/src/Components/Layouts/header.js
@@ -1,13 +1,13 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Container, Row, Col } from 'reactstrap';
 import PropTypes from 'prop-types';
-
 import { withStyles } from '@material-ui/core/styles';
 import {
-    Drawer, AppBar, Toolbar, List, Typography, IconButton, Hidden,
-    Divider, ListItem, ListItemIcon, ListItemText
+    SwipeableDrawer, AppBar, Toolbar, List, Typography, IconButton, Icon, Hidden,
+    Divider, ListItem, ListItemIcon, ListItemText, Button
   } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Icon from '@material-ui/core/Icon';
 import MenuIcon from '@material-ui/icons/Menu';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
@@ -15,14 +15,24 @@ import MailIcon from '@material-ui/icons/Mail';
 const drawerWidth = 240;
 
 const styles = theme => ({
+  list:{
+    width: 250,
+  },
   root: {
-    display: 'flex',
+    flexGrow: 1,
   },
   drawer: {
     [theme.breakpoints.up('sm')]: {
       width: drawerWidth,
       flexShrink: 0,
     },
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
   },
   appBar: {
     marginLeft: drawerWidth,
@@ -31,7 +41,6 @@ const styles = theme => ({
     },
   },
   menuButton: {
-    marginRight: 20,
     [theme.breakpoints.up('sm')]: {
       display: 'none',
     },
@@ -48,117 +57,80 @@ const styles = theme => ({
 
 class Header extends Component {
   state = {
-    mobileOpen: false,
+    isOpen: false,
   };
 
-  handleDrawerToggle = () => {
-    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+  toggleDrawer = (open) => () => {
+    this.setState({
+      isOpen: open,
+    });
   };
 
   render() {
-    const { classes, theme } = this.props;
+    const classes = this.props;
 
     const drawer = (
-      <div>
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+        <Fragment>
+          <div className={classes.toolbar} />
+          <Divider />
+          <List>
+            <ListItem>
+              <ListItemIcon>{<InboxIcon />}</ListItemIcon>
+              <ListItemText>
+                <Link to="/">Home</Link>
+              </ListItemText>
             </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+            <ListItem>
+              <ListItemIcon>{<MailIcon />}</ListItemIcon>
+              <ListItemText>
+                <Link to="/secondary">Secondary</Link>
+              </ListItemText>
             </ListItem>
-          ))}
-        </List>
-      </div>
+          </List>
+        </Fragment>
     );
 
     return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar position="fixed" className={classes.appBar}>
+      <Fragment>
+        <AppBar position="static">
           <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerToggle}
-              className={classes.menuButton}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Responsive drawer
-            </Typography>
+            <Container>
+              <Row>
+                <Col xs="1">
+                  <IconButton
+                    className={classes.menuButton, "float-left"}
+                    onClick={this.toggleDrawer(true)}
+                    color="inherit"
+                    aria-label="Menu">
+                    <MenuIcon />
+                  </IconButton>
+                </Col>
+                <Col xs="9">
+                  <Typography variant="h6" color="inherit" className={classes.grow, "text-center"}>
+                    ChefIQ
+                  </Typography>
+                </Col>
+                <Col xs="2">
+                  <Button color="inherit" className="float-right">Login</Button>
+                </Col>
+              </Row>
+            </Container>
           </Toolbar>
         </AppBar>
-        <nav className={classes.drawer}>
-          {/* The implementation can be swap with js to avoid SEO duplication of links. */}
-          <Hidden smUp implementation="css">
-            <Drawer
-              container={this.props.container}
-              variant="temporary"
-              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-              open={this.state.mobileOpen}
-              onClose={this.handleDrawerToggle}
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-              ModalProps={{
-                keepMounted: true, // Better open performance on mobile.
-              }}
+        <SwipeableDrawer
+          open={this.state.isOpen}
+          onClose={this.toggleDrawer(false)}
+          onOpen={this.toggleDrawer(true)}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer(false)}
+            onKeyDown={this.toggleDrawer(false)}
             >
-              {drawer}
-            </Drawer>
-          </Hidden>
-          <Hidden xsDown implementation="css">
-            <Drawer
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-              variant="permanent"
-              open
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-        </nav>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
-            elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
-            hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
-            Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-            viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
-            Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
-            at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
-            ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-            facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-            tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-            consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus
-            sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in.
-            In hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-            et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
-            sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo
-            viverra maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-            ultrices sagittis orci a.
-          </Typography>
-        </main>
-      </div>
+            {drawer}
+          </div>
+        </SwipeableDrawer>
+      </Fragment>
     );
   }
 }
