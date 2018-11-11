2a3aad9c0e970d4641d9ebb27b6745ef2f1883f6
austin <aturner9967@gmail.com>
2018-11-11 13:19:35 -0800

    feat: Added dropdown button for categories

--- client/src/Components/Views/Category.js
+++ client/src/Components/Views/Category.js
@@ -1,20 +1,54 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import { Container, Row, Col } from 'reactstrap';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import Item from './Item'
 
+const styles = theme => ({
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    marginLeft: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: -8,
+    },
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+})
+
 class Category extends Component {
+
   constructor (props){
     super(props);
 
     this.state = {
       categories: [],
-      colSize: null
+      colSize: null,
+      expanded: false
     };
+    this.handleExpandClick = this.handleExpandClick.bind(this);
   }
+
+  handleExpandClick() {
+    this.setState(
+      {expanded: !this.state.expanded}
+    );
+  };
 
   componentDidMount (){
     axios.get("api/v1/catalogs/" + this.props.menu)
@@ -30,6 +64,7 @@ class Category extends Component {
   }
 
   render () {
+    const { classes } = this.props;
     const categories = this.state.categories;
 
     return (
@@ -39,6 +74,16 @@ class Category extends Component {
             <Card className='categoryCard'>
               <CardContent>
                 <p>{category['name']}</p>
+                  <IconButton
+                    className={classnames(classes.expand, {
+                      [classes.expandOpen]: this.state.expanded,
+                    })}
+                    onClick={this.handleExpandClick}
+                    aria-expanded={this.state.expanded}
+                    aria-label="Show more"
+                  >
+                    <ExpandMoreIcon />
+                </IconButton>
               </CardContent>
             </Card>
           </Col>
@@ -48,4 +93,8 @@ class Category extends Component {
   }
 }
 
-export default Category;
+Category.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Category);
